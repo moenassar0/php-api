2538a38a0b27dbe7f5eedf7e2523dabd61340897
Mohamad Nassar <mnassar57@gmail.com>
2022-09-13 02:49:10 +0300

    some styling and calculation api started

--- index.php
+++ index.php
@@ -5,11 +5,27 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="styles/styles.css">
 </head>
 <body>
-    <form action="palindrome-api.php" method="GET">
-        <input name="string" type="text">
-        <input name="submit" type="submit">
-    </form>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <form action="palindrome-api.php" method="GET">
+                    <input name="string" type="text">
+                    <input name="submit" type="submit">
+                </form>
+            </div>
+            <div class="col">
+                <form action="palindrome-api.php" method="GET">
+                    <input name="a" type="text">
+                    <input name="b" type="text">
+                    <input name="c" type="text">
+                    <input name="submit" type="submit">
+                </form>
+            </div>
+        </div>
+
+    </div>
 </body>
 </html>
