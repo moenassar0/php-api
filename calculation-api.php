f500c9c830e20fb47994aeb2ff38f235034fd4ba
Mohamad Nassar <mnassar57@gmail.com>
2022-09-13 02:56:59 +0300

    calculation api implemented

--- calculation-api.php
+++ calculation-api.php
@@ -3,7 +3,19 @@
         $a = $_POST['a'];
         $b = $_POST['b'];
         $c = $_POST['c'];
-        
-        echo $a . " " . $b . " " . $c;
+        $ans = Calculate($a, $b, $c);
+        $info = array(
+            'a' =>  $a,
+            'b' => $b,
+            'c' => $c,
+            'answer' => $ans
+        );
+        $jsoninfo = json_encode($info);
+
+        echo $jsoninfo;
+    }
+
+    function Calculate($a, $b, $c){
+        return ($a * $a * $a) + ($b * $c) - ($a/$b);
     }
 ?>
